097db3c1fc0a59c909b9f6caa458983cc598578d
Pawel Wlazlo <pawel@wlazlo.dev>
2025-07-12 07:36:46 +0200

    wyprowadzenie komplecji codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,4 +1,32 @@
 return {
   "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = "InsertEnter",
   build = ":Codeium Auth",
+  opts = {
+    virtual_text = {
+      key_bindings = {
+        accept = false, -- handled by completion engine
+      },
+    },
+  },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        options = {
+          g = {
+            -- set the ai_accept function
+            ai_accept = function()
+              local item = require("codeium.virtual_text").get_current_completion_item()
+              if item then
+                vim.api.nvim_input(item.completion)
+                return true
+              end
+            end,
+          },
+        },
+      },
+    },
+  },
 }
